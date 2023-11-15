673100fe886fab32489a1cc0edd9389f0b3691c8
VictorPadilla0861 <146771873+VictorPadilla0861@users.noreply.github.com>
2023-11-15 11:13:09 -0800

    added mountain card, needs image tendiing

--- Capstone Template Incomplete/scripts/mountains.js
+++ Capstone Template Incomplete/scripts/mountains.js
@@ -1,46 +1,45 @@
-//  submitButton.addEventListener('click', async () => {
-
-//  });
-
-function mountainTemplate(mountain) {
-  //let mySunrise = getSunsetForMountain(`${mountain.coords.lat},${mountain.coords.lng}`);
-
-  return `
-    <div class="mountain">
-    <img class="mountain-photo" src="images/${mountain.img}">
-    <h2 class="mountain-name">${mountain.name} <span class="species">(${mountain.elevation} feet)</span></h2>
-    <h4 class="mountain-desc">${mountain.desc} </h4>
-    <p><strong>Effort:</strong> ${mountain.effort}</p>
-    <strong>Coordinates:</strong> lat: ${mountain.coords.lat} lng: ${mountain.coords.lng}
-    <strong>Sunrise:</strong> ${mySunrise}
-    </div>
-    `;
-}
-
-document.getElementById("mountains").innerHTML = `
-  <h1 class="app-title"> ${mountainsArray.length} Mountains to climb</h1>
-  ${mountainsArray.map(mountainTemplate).join("")}
-  <p class="footer">These ${
-    mountainsArray.length
-  } mountants were added recently. Check back soon for updates.</p>
-`;
+// fetch the sunrise/sunset times for a specific mountain
 
 async function getSunsetForMountain(lat, lng) {
-  let response = await fetch(
-    `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`
-  );
+  let response = await fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`);
   let data = await response.json();
-  return data;
+  return data.results;
 }
 
-function loadData() {
-  var down = document.getElementById("mountain");
+// function to populate the dropdown list with mountain names
+
+function populateDropdown() {
+  const mountainSelect = document.getElementById('mountain-select');
+  
   for (let i = 0; i < mountainsArray.length; i++) {
-    var optn = mountainsArray[i];
-    var el = document.createElement("option");
-    el.textContent = optn;
-    el.value = optn;
-    down.appendChild(el);
+    const mountainOption = document.createElement('option');
+    mountainOption.value = i;
+    mountainOption.text = mountainsArray[i].name;  //i=0 to 47
+    mountainSelect.appendChild(mountainOption);
   }
-  down.innerHTML = "Elements Added";
 }
+
+// fuction to display the selected mountain information
+
+function displayMountainInfo() {
+  const mountainIndex = document.getElementById('mountain-select').value;
+  const mountain = mountainsArray[mountainIndex];
+
+  document.getElementById('mountain-name').innerHTML = `<p> <strong> Name:</strong> ${mountain.name} </p>`;
+  document.getElementById('mountain-description').innerHTML =`<p> <strong> Description: </strong>  ${mountain.desc} </p>`;
+  document.getElementById('mountain-elevation').innerHTML = `<p> <strong> Elevation: </strong> ${mountain.elevation} meters </p>`;
+  document.getElementById('mountain-effort').innerHTML = `<p> <strong> Effort: </strong> ${mountain.effort} </p>`;
+  document.getElementById('mountain-image').setAttribute('src', mountain.img);
+
+  getSunsetForMountain(mountain.lat, mountain.lng).then(data => {
+    document.getElementById('sunrise-time').innerHTML = `<p> <strong> Sunrise (UTC): </strong> ${data.sunrise} </p>`;
+    document.getElementById('sunset-time').innerHTML = `<p> <strong> Sunset (UTC): </strong> ${data.sunset} </p>`;
+  });
+}
+
+
+// event listener when the dropdown selection changes
+document.getElementById('mountain-select').addEventListener('change', displayMountainInfo);
+
+// populate the dropdown list 
+populateDropdown();

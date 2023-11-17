b5ba309b13282481bcafb21647f91e58dc237501
VictorPadilla0861 <146771873+VictorPadilla0861@users.noreply.github.com>
2023-11-17 10:11:34 -0800

    edited line

--- Capstone Template Incomplete/scripts/app.js
+++ Capstone Template Incomplete/scripts/app.js
@@ -9,14 +9,10 @@ window.onload = () => {
 };
 
 function addLocationsToDropdown() {
-    // Get the dropdown element by its ID
 
-    // Use forEach to iterate over the array
     locationsArray.forEach(function (location) {
-    // Create a new option element
     let option = document.createElement("option");
 
-    // Set the value and text content for the new option
     option.value = location;
     option.textContent = location;
 
@@ -26,23 +22,17 @@ function addLocationsToDropdown() {
 }
 
 function addNationalParksToDropdown(){
-        // Get the dropdown element by its ID
 
-    // Use forEach to iterate over the array
     locationsArray.forEach(function (park) {
-        // Create a new option element
         let option = document.createElement("option");
     
-        // Set the value and text content for the new option
         option.value = park.LocationName;
         option.textContent = park.LocationName;
     
-        // Add the new option to the dropdown
         parktype.appendChild(option);
         });
 }
 
-//Grab the value of a selected value from a dropdown
 function getSelectedStateValue(){
     const selectedValue = states.value;
 }
